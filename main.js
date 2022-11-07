da324ea2223495fcd226ba7c1229335deba38b8e
Henrique Bianchini Moreno <henriqueoze@gmail.com>
2022-11-06 20:54:50 -0400

    created btn for color change

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ function createGame(player1, hour, player2) {
   `
 }
 
-
 let delay = -0.3;
 
 function createCard(date, day, games) {
@@ -29,3 +28,53 @@ document.querySelector('#cards').innerHTML = `
     ${createCard("28/11", "segunda", createGame('brazil', '13:00', 'switzerland') + createGame('portugal', '16:00', 'uruguai'))}
     ${createCard("02/12", "sexta", createGame('brazil', '16:00', 'cameroon'))}
 `
+
+
+
+
+let btn_green = document.querySelector('#btn-green')
+let btn_blue = document.querySelector('#btn-blue')
+let btn_yellow = document.querySelector('#btn-yellow')
+
+let body = document.querySelector('body')
+
+
+function changeColorYellow(){
+  body.className = 'yellow'
+  checkColor()
+} 
+function changeColorBlue(){
+  body.className = 'blue'
+  checkColor()
+} 
+function changeColorGreen(){
+  body.className = 'green'
+  checkColor()
+} 
+
+checkColor()
+
+
+function checkColor(){
+
+  if (body.classList.contains('blue')){
+
+    btn_green.style.filter = "brightness(100%)"
+    btn_yellow.style.filter = "brightness(100%)"
+    btn_blue.style.filter = "brightness(20%)"
+    
+  }else if(body.classList.contains('green')){
+
+    btn_blue.style.filter = "brightness(100%)"
+    btn_yellow.style.filter = "brightness(100%)"
+    btn_green.style.filter = "brightness(20%)"
+    
+  }else if(body.classList.contains('yellow')){
+    
+    btn_blue.style.filter = "brightness(100%)"
+    btn_green.style.filter = "brightness(100%)"
+    btn_yellow.style.filter = "brightness(20%)"
+    
+  }
+  
+}
